11d22cc8a3e70fbcf8b6a8a18ecd3d980677fb7b
ale <aa.olivera@hotmail.com>
2021-12-27 22:58:06 -0300

    altimetro

--- 2.txt.ino
+++ 2.txt.ino
@@ -1,21 +1,28 @@
 #include <Adafruit_MPU6050.h>
+#include <Adafruit_BMP085.h>
 #include <SD.h>
 #define CS_PIN 10
 
 short int  periodo = 100;
 unsigned long long int tiempoAhora = 0;
 Adafruit_MPU6050 mpu;
+Adafruit_BMP085 bmp;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
   setupAcelerometro();
+  setupAltimetro();
   setupMemory();
 }
 
 void setupAcelerometro() {
   while (!mpu.begin()) delay(100);
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
+}
+
+void setupAltimetro() {
+  while (!bmp.begin()) delay(100);
 }
 
 void setupMemory() {
@@ -29,8 +36,10 @@ void setupMemory() {
 
 void loop() {
   if (millis() > tiempoAhora + periodo) {
-    String dataString = "";
+    String dataString = "{t:" + String(millis());
     readAceleracion(dataString);
+    readAltimetro(dataString);
+    dataString += "}";
     Serial.println(dataString);
 
     File dataFile = SD.open("info.txt", FILE_WRITE);
@@ -52,12 +61,16 @@ void loop() {
 void readAceleracion(String& dataString) {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  dataString += "{t:" + String(millis());
-  dataString += ",temp:" + String(temp.temperature);
-  dataString += ",a:{ X:" + String(a.acceleration.x);
-  dataString += ",Y:" + String(a.acceleration.y);
-  dataString += ",Z:" + String(a.acceleration.z);
-  dataString += "},g:{ X:" + String(g.gyro.x);
-  dataString += ",Y:" + String(g.gyro.y);
-  dataString += ",Z:" + String(g.gyro.z) + "}}";
+  dataString += ",\"temp\":" + String(temp.temperature);
+  dataString += ",\"a\":{\"X\":" + String(a.acceleration.x);
+  dataString += ",\"Y\":" + String(a.acceleration.y);
+  dataString += ",\"Z\":" + String(a.acceleration.z) + "}";
+  dataString += ",\"g\":{\"X\":" + String(g.gyro.x);
+  dataString += ",\"Y\":" + String(g.gyro.y);
+  dataString += ",\"Z\":" + String(g.gyro.z) + "}";
+}
+
+void readAltimetro(String& dataString) {
+  dataString += ",\"p\":{\"P\":" + String(bmp.readPressure());
+  dataString += ",\"A\":" + String(bmp.readAltitude()) + "}";
 }
